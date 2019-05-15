d26de26793159ace3b2bf20de7c36c8292029d6d
Harmelodic <matt@harmelodic.com>
2019-05-15 18:14:55 +0100

    better sql interface

--- src/db.js
+++ src/db.js
@@ -1,16 +1,8 @@
-const { sql } = require("@harmelodic/interfaces");
-const config = require("./db_config.json");
-
-const db = {};
-
-sql.open(config);
-
-db.query = query => {
-    return sql.query(query)
-}
-
-db.close = () => {
-    sql.close();
-}
-
-module.exports = db;
+module.exports = require("@harmelodic/interfaces").sql({
+    host: "localhost",
+    port: 3306,
+    database: "imdb",
+    user: "root",
+    password: "",
+    connectionLimit: 100
+})
