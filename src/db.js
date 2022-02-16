54618dc0cff6b8f6ece555ce073a5ab18ab25e96
Harmelodic <matt@harmelodic.com>
2022-02-16 17:28:30 +0100

    Replacing interfaces with mysql script

--- src/db.js
+++ src/db.js
@@ -1,8 +1,40 @@
-module.exports = require("@harmelodic/interfaces").sql({
-    host: "localhost",
-    port: 3306,
-    database: "imdb",
-    user: "root",
-    password: "",
-    connectionLimit: 100
-})
+const mysql = require('mysql');
+
+const sql = {};
+let pool;
+
+sql.close = () => {
+	if (pool) pool.end();
+	pool = null;
+};
+
+sql.query = (sql) => {
+	return new Promise(((resolve, reject) => {
+		pool.getConnection((err, con) => {
+			if (err) reject(err);
+
+			con.query(sql, (error, results) => {
+				if (error) reject(error);
+
+				con.release();
+				resolve(results);
+			});
+		});
+	}));
+};
+
+process.on('beforeExit', () => {
+	sql.close();
+});
+
+module.exports = () => {
+	pool = mysql.createPool({
+        host: "localhost",
+        port: 3306,
+        database: "imdb",
+        user: "root",
+        password: "",
+        connectionLimit: 100
+    });
+	return sql;
+};
